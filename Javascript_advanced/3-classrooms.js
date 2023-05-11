f607f41f9b8d35012ec2ea246f2fd70b7cdefa86
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-05-11 17:14:25 +0100

    JS

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,13 @@
+function createClassRoom (numberOfStudents);{
+    function studentSeat (seat);
+    return seat;
+    let students = [];
+    for (let j = 0; j < numberOfStudents; j++) 
+        students.push(studentSeat(i + 1)); {
+    }
+    return students;
+  }
+  let classRoom = createClassRoom(10);
+    }
+}
+
