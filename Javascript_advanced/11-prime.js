923c9abb12766577acfad8747291c599dbb8f083
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-05-12 16:50:59 +0100

    JS

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,24 @@
+function countPrimeNumbers() {
+    let counter = 0;
+    for (let i = 2; i <= 100; i++) {
+        let flag = true;
+
+        for(let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            counter +=1;
+        }
+    }
+    return counter + 1;
+}
+let time = 0;
+setTimeout(() => {console.log("Execution time of printing countPrimeNumbers was " + time +  " milliseconds.");})
+let x = performance.now();
+for ( i = 0; i < 100; ++i) {
+    countPrimeNumbers();
+}
+let y = performance.now();
+time = y - x;
